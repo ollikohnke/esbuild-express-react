cc21bbc688309862fabe62544af9150640900405
ollikohnke <ollikohnke@gmail.com>
2024-11-20 16:28:50 +0200

    More fixes to env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const dotenv = require('dotenv')
-dotenv.config({ path: `${__dirname}/.dot.env` });
+dotenv.config({ path: `${__dirname}/.env` });
 
 const app = express()
 
